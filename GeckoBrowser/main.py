b1098f6602a34d2cea4e0815c8c810a79d4294db
Jonas Pauli <jonaspauli089@gmail.com>
2021-03-11 12:05:06 +0100

    Progress Bars
    + Fixed 1 syntax error(s)
    + Fixed indices

--- GeckoBrowser/main.py
+++ GeckoBrowser/main.py
@@ -4,16 +4,17 @@ import time
 import pickle
 from os import system, name
 import os
+from tqdm import tqdm, trange
 
 list = 'https://api.coingecko.com/api/v3/coins/list'
 tickers = 'https://api.coingecko.com/api/v3/coins/'
 
 def clear():
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
-
+    #if name == 'nt':
+    #    _ = system('cls')
+    #else:
+    #    _ = system('clear')
+    pass
 def cutx(string, x):
     res = ''
     loc = 1000000
@@ -72,47 +73,41 @@ def FilterByAge(yt, ids):
     Etimeout = 'Expecting value: line 1 column 1 (char 0)'
     st = time.time()
     coins = []
-    for e in range(0, len(ids) - 1):
-        et = cut(str(time.time() - st))
-        print('[Searching for coins that were listed on Coingecko less than ' + str(yt) + ' days ago]')
-
-        print('Runtime(s): ' + et)
-        if e > 0:
-            toi = float(et) / e
-            tl = toi * (len(ids) - 1 - e)
-            print('Est. Time left: ' + cut(str(tl)) + 's' + ' (= ' + cutx(str(tl/60/60), 3) + 'h' + ')')
-
-        print('[' + str(e + 1) + '/' + str(len(ids) - 1) + ']')
-        i = ids[e]
-        try:
-            r = requests.get(tickers + i + '/market_chart?vs_currency=usd&days=' + str(yt) + '&interval=daily')
-            res = json.loads(r.text)
-            if len(res['prices']) < yt:
-                coins.append(len(coins))
-                coins[len(coins) - 1] = i
-            else:
-                old_coins.append(len(old_coins))
-                old_coins[len(old_coins) - 1] = i
-        except Exception as E:
-            if str(E) == Etimeout:
-                while True:
-                    try:
-                        r = requests.get(tickers + i + '/market_chart?vs_currency=usd&days=' + str(yt) + '&interval=daily')
-                        res = json.loads(r.text)
-                        if len(res['prices']) < yt:
-                            coins.append(len(coins))
-                            coins[len(coins) - 1] = i
-                        else:
-                            old_coins.append(len(old_coins))
-                            old_coins[len(old_coins) - 1] = i
-                        break
-                    except Exception as E:
-                        print('Warning: ' + str(E))
-                        print('[This is due to too many requests, this Warning is resolved automatically.]')
-                        time.sleep(10)
-            else:
-                print('Error: ' + str(E))
-                time.sleep(10)
+    print('[Searching for coins that were listed on Coingecko less than ' + str(yt) + ' days ago]')
+    with tqdm(total=len(ids)) as pbar:
+        for e in range(0, len(ids)):
+            pbar.update(1)
+            et = cut(str(time.time() - st))
+            if e > 0:
+                toi = float(et) / e
+                tl = toi * (len(ids) - 1 - e)
+            i = ids[e]
+            try:
+                r = requests.get(tickers + i + '/market_chart?vs_currency=usd&days=' + str(yt) + '&interval=daily')
+                res = json.loads(r.text)
+                if len(res['prices']) < yt:
+                    coins.append(len(coins))
+                    coins[len(coins) - 1] = i
+                else:
+                    old_coins.append(len(old_coins))
+                    old_coins[len(old_coins) - 1] = i
+            except Exception as E:
+                if str(E) == Etimeout:
+                    while True:
+                        try:
+                            r = requests.get(tickers + i + '/market_chart?vs_currency=usd&days=' + str(yt) + '&interval=daily')
+                            res = json.loads(r.text)
+                            if len(res['prices']) < yt:
+                                coins.append(len(coins))
+                                coins[len(coins) - 1] = i
+                            else:
+                                old_coins.append(len(old_coins))
+                                old_coins[len(old_coins) - 1] = i
+                            break
+                        except Exception as E:
+                            time.sleep(10)
+                else:
+                    time.sleep(10)
         clear()
     os.remove('OLD_' + str(days) + '.dat')
     open('OLD_' + str(days) + '.dat', 'x')
@@ -122,27 +117,28 @@ def FilterByAge(yt, ids):
 
 def XgapFix(Range, acceptance, data):
     coins = FilterByAge(Range, data)
-    for i in range(0, len(data)):
-        while True:
-            try:
-                r = requests.get(tickers + i + '/market_chart?vs_currency=usd&days=' + str(Range) + '&interval=daily')
-                res = json.loads(r.text)
-                if len(res['prices']) < acceptance:
-                    coins.append(len(coins))
-                    coins[len(coins) - 1] = i
-                else:
-                    old_coins.append(len(old_coins))
-                    old_coins[len(old_coins) - 1] = i
-                    break
-            except Exception as E:
-                print('Warning: ' + str(E))
-                print('[This is due to too many requests, this Warning is resolved automatically.]')
-                time.sleep(10)
+    print('[Applying XgapFix to list of ' + str(len(data)) + ' coins]')
+    with tqdm(total=len(coins)) as pbar:
+        for i in range(0, len(data)):
+            pbar.update(1)
+            while True:
+                try:
+                    r = requests.get(tickers + data[i] + '/market_chart?vs_currency=usd&days=' + str(Range) + '&interval=daily')
+                    res = json.loads(r.text)
+                    if len(res['prices']) < acceptance:
+                        coins.append(len(coins))
+                        coins[len(coins) - 1] = data[i]
+                    else:
+                        old_coins.append(len(old_coins))
+                        old_coins[len(old_coins) - 1] = data[i]
+                        break
+                except Exception as E:
+                    time.sleep(10)
     os.remove('OLD_' + str(days) + '.dat')
     open('OLD_' + str(days) + '.dat', 'x')
     with open('OLD_' + str(days) + '.dat', 'wb') as database:
             pickle.dump(old_coins, database)
-            
+
     return coins
 
 def unkown_vol_out(data):
@@ -151,67 +147,55 @@ def unkown_vol_out(data):
     Enovolume = 'Expecting value: line 1 column 1 (char 0)'
     uke = 0
     st = time.time()
-    for i in range(0, len(data)):
-        et = cut(str(time.time() - st))
-        print('[Removing coins with an unknown 24 hour volume]')
-        print('Runtime(s): ' + et)
-        print('[' + str(i + 1) + '/' + str(len(data)) + ']')
-        c = data[i]
-        uvo = 'https://api.coingecko.com/api/v3/simple/price?ids=' + c + '&vs_currencies=usd&include_24hr_vol=true'
-        try:
-            r = requests.get(uvo)
-            res = json.loads(r.text)
+    print('[Applying Unkown-Volume-Filter to list of ' + str(len(data)) + ' coins]')
+    with tqdm(total=len(data)) as pbar:
+        for i in range(0, len(data)):
+            pbar.update(1)
+            et = cut(str(time.time() - st))
+            #print('[' + str(i + 1) + '/' + str(len(data)) + ']')
+            c = data[i]
+            uvo = 'https://api.coingecko.com/api/v3/simple/price?ids=' + c + '&vs_currencies=usd&include_24hr_vol=true'
             try:
-                d = float(res[c]['usd_24h_vol'])
-                if d > 1:
-                    print(str(d))
-
-                    coins.append(len(coins))
-                    coins[len(coins) - 1] = c
-                    print('[valid coin]')
-
-            except Exception as NOFLOAT:
-                uke += 1
-                print('[invalid coin]')
-                pass
-            clear()
-        except Exception as E:
-            while True:
+                r = requests.get(uvo)
+                res = json.loads(r.text)
                 try:
-                    uvo = 'https://api.coingecko.com/api/v3/simple/price?ids=' + c + '&vs_currencies=usd&include_24hr_vol=true'
-                    r = requests.get(uvo)
-                    res = json.loads(r.text)
+                    d = float(res[c]['usd_24h_vol'])
+                    if d > 1:
+                        coins.append(len(coins))
+                        coins[len(coins) - 1] = c
+
+                except Exception as NOFLOAT:
+                    uke += 1
+                    pass
+                clear()
+            except Exception as E:
+                while True:
                     try:
-                        d = float(res[c]['usd_24h_vol'])
-                        if d > 1:
-                            print('[valid coin]')
-
-                            coins.append(len(coins))
-                            coins[len(coins) - 1] = c
-                    except Exception as NOFLOAT:
-                        uke += 1
-                        print('[invalid coin]')
-
-                        pass
-                    clear()
-                    break
-                except Exception as E2:
-                    if str(E2) == Etimeout:
-                        print('[Warning: Known timeout Error, will be fixed automatically]')
-                        time.sleep(10)
-                    elif Enovolume in str(E2):
-                        uke += 1
-                        print('[invalid coin]')
-
+                        uvo = 'https://api.coingecko.com/api/v3/simple/price?ids=' + c + '&vs_currencies=usd&include_24hr_vol=true'
+                        r = requests.get(uvo)
+                        res = json.loads(r.text)
+                        try:
+                            d = float(res[c]['usd_24h_vol'])
+                            if d > 1:
+                                coins.append(len(coins))
+                                coins[len(coins) - 1] = c
+                        except Exception as NOFLOAT:
+                            uke += 1
+                            pass
                         clear()
                         break
-                    else:
-                        uke += 1
-                        print('[invalid coin]')
-
-                        time.sleep(10)
-                        clear()
-                        break
+                    except Exception as E2:
+                        if str(E2) == Etimeout:
+                            time.sleep(10)
+                        elif Enovolume in str(E2):
+                            uke += 1
+                            clear()
+                            break
+                        else:
+                            uke += 1
+                            time.sleep(10)
+                            clear()
+                            break
     print('Unkown Errors [ignored]: ' + str(uke))
     time.sleep(10)
     return coins
@@ -230,12 +214,15 @@ def ExchangeFilterV1(coin, exchange):
 
 def ExchangeFilterV2(coins, exchange):
     result = []
-    for i in range(0, len(coins) - 1):
-        print('[' + str(i) + '/' + str(len(coins) - 1) + ']')
-        c = coins[i]
-        if ExchangeFilterV1(c, exchange) == True:
-            result.append(len(result))
-            result[len(result) - 1] = c
+    print('[Applying Exchange-Filter to list of ' + str(len(coins)) + ' coins]')
+    with tqdm(total=len(coins)) as pbar:
+        for i in range(0, len(coins)):
+            pbar.update(1)
+            #print('[' + str(i) + '/' + str(len(coins) - 1) + ']')
+            c = coins[i]
+            if ExchangeFilterV1(c, exchange) == True:
+                result.append(len(result))
+                result[len(result) - 1] = c
     return result
 
 def NewOnGecko(days):
@@ -247,7 +234,6 @@ def ReturnFileData(days, date):
         with open('db_d' + str(days) + '_' + date + '.dat', 'rb') as database:
             data = pickle.load(database)
     except Exception as empty:
-        print(str(empty))
         pass
     return data
 
@@ -266,15 +252,18 @@ def ResetAndCheckAll(days, date):
 
 def FilterByVolumeRange(bottom, top, coins):
     result = []
-    for c in range(0, len(coins) - 1):
-        i = coins[c]
-        url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=' + i + '&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h'
-        r = requests.get(url)
-        res = json.loads(r.text)
-        volume = float(res['total_volume'])
-        if volume >= bottom and volume <= top:
-            result.append(len(result))
-            result[len(result) - 1] = i
+    print('[Applying Volume-Filter to list of ' + str(len(coins)) + ' coins]')
+    with tqdm(total=len(coins)) as pbar:
+        for c in range(0, len(coins)):
+            pbar.update(1)
+            i = coins[c]
+            url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=' + i + '&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h'
+            r = requests.get(url)
+            res = json.loads(r.text)
+            volume = float(res['total_volume'])
+            if volume >= bottom and volume <= top:
+                result.append(len(result))
+                result[len(result) - 1] = i
     return result
 
 def OnExchangeMinVol(data, exchange, minvol, maxvol):
